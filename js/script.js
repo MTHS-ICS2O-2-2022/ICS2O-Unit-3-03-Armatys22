7db029a8d58dce2dd4e34fb47aa7f9496d01d404
Armatys22 <124389724+Armatys22@users.noreply.github.com>
2023-04-10 21:24:49 +0000

    fghfyjguyg

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: dom
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function calculates volume of a sphere.
+ */
+function calculate() {
+  // input
+  const radius = parseInt(document.getElementById("radius-of-sphere").value)
+
+  // process
+  const volume = 3/4 * 3.14 * radius
+
+  // output
+  document.getElementById("volume").innerHTML = "volume is: " + volume + " mm³"
+}
